96d34796ae428436ec0b818a4f7fb2ab3512a937
data_agile <niklas3000@gmail.com>
2023-03-12 16:02:55 +0100

    rättning

--- llm_app.py
+++ llm_app.py
@@ -7,7 +7,7 @@ import os
 st.set_page_config(page_title="Rätta och förklara", page_icon=":robot:")
 st.header("Rätta och förklara skrivfel")
 
-st.markdown("Se vilka fel i stavning och grammatik en text har. Få den korrekta texten utskriven. Appen använder Open AI:s modell gpt-3.5-turbo.")
+st.markdown("Se vilka fel i stavning och grammatik en text har. Få den korrekta texten utskriven. Appen använder Open AI-modellen gpt-3.5-turbo.")
 st.markdown("## Skriv in text att rätta")
 
 
